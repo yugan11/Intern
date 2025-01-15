de57ad7b6bf0a7abe987ae0ce346b4885e392552
YUGAN S <yug064793@gmail.com>
2025-01-15 12:34:34 +0530

    Add files via upload

---
+++ average_tuples.py
@@ -0,0 +1,3 @@
+tuple=((10,10,10,12),(30,45,56,45),(81,80,39,32),(1,2,3,4))
+average = [sum(tup) / len(tup) for tup in tuple]
+print("Average values of the numbers of the said tuples of tuple :",average)
